f119da7ff75f371fd9cca638489e11874347b297
SvenHSSL <102971898+SvenHSSL@users.noreply.github.com>
2022-04-04 16:54:33 +0200

    Add files via upload

--- first.py
+++ first.py
@@ -2,13 +2,13 @@ from tkinter import *
 
 class Window(Frame):
     def __init__(self, master=None):
-        root.geometry('300x300')
+        root.geometry('600x600')
         Frame.__init__(self, master)
         self.master = master
-        Label(root,text ="Bienvenue dans YTDown, taper le nom de la vidéo que vous voulez télécharger ")
-        Label(root,text = "URL" ).grid(row =0)
+        Label(root,text ="Bienvenue dans YTDown, taper le nom de la vidéo que vous voulez télécharger ").grid(row =0, column= 5)
+        Label(root,text = "URL" ).grid(row =1)
         ent1= Entry(root)
-        ent1.grid(row=0, column=1) 
+        ent1.grid(row=1, column=1) 
 # initialize tkinter
 root = Tk()
 app = Window(root)
